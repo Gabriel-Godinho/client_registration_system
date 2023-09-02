c09db608daafd77c4dd0874479187f1627f99390
Gabriel-Godinho <gabriel234417@gmail.com>
2023-09-02 15:46:27 -0300

    Unnecessary prints removed

--- src/main/java/br/univille/Application.java
+++ src/main/java/br/univille/Application.java
@@ -5,7 +5,6 @@ import br.univille.login.view.LoginView;
 public class Application {
     public static void main(String[] args) {
         LoginView view = new LoginView();
-        System.out.println(view.getAlignmentX());
-        System.out.println(view.getAlignmentY());
     }
+
 }
